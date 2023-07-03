97a4b4bfec02d193c78e1accc86944a575e8a7b8
AppleVine <jakebco99@icloud.com>
2023-07-03 23:55:01 +1000

    login html & css created.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,46 @@
+import React, { useState } from 'react';
 import Header from "../components/header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import "./login.css"
 
-export default function Loginpage() {
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+    
+  // };
+
   return (
     <div>
       <Header />
-      <h1>This is the login page</h1>
+      <div className="login">
+        <form>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input type="text" className="form-control" id="exampleFormControlInput1" value={username} onChange={handleUsernameChange} />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleFormControlInput2" value={password} onChange={handlePasswordChange} />
+          </div>
+          
+          <Button type="submit" variant="primary">Login</Button>
+        </form>
+  
+        <Button className="account" variant="primary">Account Creation</Button>
+      </div>
     </div>
   );
+  
 }
