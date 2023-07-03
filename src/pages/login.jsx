e5a00da33b2bacb6338e1d9c7cb1087a36090566
AppleVine <jakebco99@icloud.com>
2023-07-03 17:03:41 +1000

    Created header component and base of login page.

---
+++ src/pages/login.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+
+export default function Loginpage() {
+  return (
+    <div>
+      <Header />
+      <h1>This is the login page</h1>
+    </div>
+  );
+}
