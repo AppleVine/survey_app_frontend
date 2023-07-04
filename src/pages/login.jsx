2fa86b9a34c5c839ee49df9e55080eb828ff8433
AppleVine <jakebco99@icloud.com>
2023-07-04 14:23:00 +1000

    login page logs JSON user info. Waiting for API endpoint.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,23 +8,36 @@ export default function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+const handleUsernameChange = (event) => {
+  setUsername(event.target.value);
+};
+
+const handlePasswordChange = (event) => {
+  setPassword(event.target.value);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+  if (!passwordRegex.test(password)) {
+    // Display an error message or take appropriate action for invalid password
+    return;
+  }
+
+  const userData = {
+    "email": username,
+    "password": password,
   };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-    
-  // };
+  console.log(JSON.stringify(userData));
+  
+};
 
   return (
     <div>
       <Header />
-      <div className="login">
+      <div className="login"> 
         <form>
           <div className="mb-3">
             <label className="form-label">Username</label>
@@ -34,8 +47,7 @@ export default function LoginPage() {
             <label className="form-label">Password</label>
             <input type="password" className="form-control" id="exampleFormControlInput2" value={password} onChange={handlePasswordChange} />
           </div>
-          
-          <Button type="submit" variant="primary">Login</Button>
+          <Button type="submit" variant="primary" onClick={handleSubmit}>Login</Button>
         </form>
   
         <Button className="account" variant="primary">Account Creation</Button>
