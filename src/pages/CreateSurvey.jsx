dd6ba3ee5947eaddb3efc3df61f4b16fe87ef2e2
Alexandria McPherson <alexandrialeemcpherson@gmail.com>
2023-07-20 04:20:04 +0900

    Make CreateSurvey and EditSurvey redirect to login page if not logged in

--- src/pages/CreateSurvey.jsx
+++ src/pages/CreateSurvey.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import EditSurveyContainer from '../components/createSurvey/EditSurveyContainer';
+import { checkLoginAndRedirect } from '../services/authServices';
 
 export default function CreateSurvey() {
+
+  // Redirect to login page if not logged in
+  useEffect(() => {
+    checkLoginAndRedirect();
+  }, []);
 
   return (
     <div>
--- src/pages/EditSurvey.jsx
+++ src/pages/EditSurvey.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import { useSurveyDispatchContext } from '../components/createSurvey/surveyContext';
 import EditSurveyContainer from '../components/createSurvey/EditSurveyContainer';
 import { getSurvey } from '../services/surveyServices';
+import { checkLoginAndRedirect } from '../services/authServices';
 
 export default function EditSurvey() {
+
+  // Redirect to login page if not logged in
+  useEffect(() => {
+    checkLoginAndRedirect();
+  }, []);
+
   // Get survey id from url
   let surveyId = useParams();
   // Load data from db
