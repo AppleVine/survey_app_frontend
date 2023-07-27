b0c6a6acc0d0ae6d8f08705791067f38024a0cee
Alexandria McPherson <alexandrialeemcpherson@gmail.com>
2023-07-28 07:18:25 +0900

    Style View Survey page

--- src/components/survey/EditSurveyButton.jsx
+++ src/components/survey/EditSurveyButton.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { checkForUser } from '../../services/authServices'
 
+// CSS imports
+import Button from 'react-bootstrap/Button';
+
 export default function EditSurveyButton() {
  const {surveyId} = useParams();
  const isUser = checkForUser();
 
   return (
-    <div>
+    <div className='d-flex justify-content-center'>
       {
-        isUser ? <Link to={`/surveys/${surveyId}/edit`}>Edit Survey</Link>: null
+        isUser ? <Link to={`/surveys/${surveyId}/edit`}><Button variant="primary">Edit Survey</Button></Link>: null
       }
     </div>
   )
--- src/components/survey/ViewSurveyContainer.jsx
+++ src/components/survey/ViewSurveyContainer.jsx
@@ -6,6 +6,12 @@ import SurveyIntroText from './SurveyIntroText'
 import SurveyQuestionsContainer from './SurveyQuestionsContainer'
 import EditSurveyButton from './EditSurveyButton'
 
+// CSS imports
+import './EditViewSurveyContainer.css'
+import Stack from 'react-bootstrap/Stack'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 export default function ViewSurveyContainer() {
   const editDispatch = useEditDispatchContext();
 
@@ -15,12 +21,16 @@ export default function ViewSurveyContainer() {
   }, []);
 
   return (
-    <div>
-        <SurveyTitle />
-        <SurveyDescription />
-        <SurveyIntroText />
-        <SurveyQuestionsContainer />
-        <EditSurveyButton />
-    </div>
+<Row className='justify-content-center'>
+  <Col md={10}>
+    <Stack gap={3} className='survey-container'>
+      <SurveyTitle />
+      <SurveyDescription />
+      <SurveyIntroText />
+      <SurveyQuestionsContainer />
+      <EditSurveyButton />
+    </Stack>
+  </Col>
+</Row>
   )
 }
