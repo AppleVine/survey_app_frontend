6ce58847984b7421a177267d82914ccbd0d1017c
Jakeb <Jakebco99@icloud.com>
2023-08-02 15:45:01 +1000

    Updated viewSurveys UI, display surveys and responses in order of date. Added date display.

--- src/pages/SurveyResponses.jsx
+++ src/pages/SurveyResponses.jsx
@@ -33,6 +33,8 @@ export default function SurveyResponses() {
     const fetchResponses = async () => {
       try {
         const surveyResponses = await getSurveyResponses(surveyID);
+        // Sort the surveyResponses array based on dateSubmitted in descending order
+        surveyResponses.sort((a, b) => new Date(b.dateSubmitted) - new Date(a.dateSubmitted));
         setResponses(surveyResponses);
       } catch (error) {
         console.error(error);
@@ -55,6 +57,11 @@ export default function SurveyResponses() {
     fetchSurvey();
   }, [surveyID]);
 
+  const formatDate = (date) => {
+    const formattedDate = new Date(date).toLocaleDateString("en-GB");
+    return formattedDate;
+  };
+
   return (
     <div>
       <Header />
@@ -68,15 +75,17 @@ export default function SurveyResponses() {
         {responses.map((response) => (
           <div key={response._id} className="response-tile">
             <ul>
+              <li>
+                <h6>{formatDate(response.dateSubmitted)}</h6>
+              </li>
               {survey.questions && Array.isArray(survey.questions) && survey.questions.map((question, index) => (
                 <li key={index}>
                   <strong>Question {index + 1}: </strong>
                   {question.questionText}
                   <br />
-                  { typeof response.answers[index] === 'string' ?
-                  response.answers[index]
-                  :
-                  parseMultiChoice(response.answers[index])
+                  {typeof response.answers[index] === 'string'
+                    ? response.answers[index]
+                    : parseMultiChoice(response.answers[index])
                   }
                 </li>
               ))}
--- src/pages/ViewSurveys.jsx
+++ src/pages/ViewSurveys.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Header from "../components/header";
 import { getSurveys } from "../services/surveyServices";
 import { checkLoginAndRedirect } from '../services/authServices';
-
-// CSS imports
 import Button from 'react-bootstrap/Button';
 import "./ViewSurveys.css";
 
@@ -20,6 +17,8 @@ export default function ViewSurveys() {
     const fetchSurveys = async () => {
       try {
         const theSurveys = await getSurveys();
+        // Sort the surveys array based on dateSubmitted in descending order
+        theSurveys.sort((a, b) => new Date(b.dateSubmitted) - new Date(a.dateSubmitted));
         setSurveys(theSurveys);
       } catch (error) {
         console.error(error);
@@ -28,6 +27,12 @@ export default function ViewSurveys() {
     fetchSurveys();
   }, []);
 
+  // format date to dd/mm/yy
+  const formatDate = (date) => {
+    const formattedDate = new Date(date).toLocaleDateString("en-GB");
+    return formattedDate;
+  };
+
   return (
     <div>
       <Header />
@@ -35,15 +40,19 @@ export default function ViewSurveys() {
         {surveys.map((survey) => (
           <div key={survey._id} className="survey-title">
             <div className="title-id">
-              <Link className="survey-link" to={`/surveys/${survey._id}`}>
                 <h2>{survey.title}</h2>
-              </Link>
-              <h6>{survey._id}</h6>
+              <div className="id-date-container">
+                <h6>{survey._id}</h6>
+                <h6>{formatDate(survey.dateSubmitted)}</h6>
+              </div>
             </div>
             <div className="survey-info">
               <p>{survey.description}</p>
               <h6>Responses: {survey.responses.length}</h6>
+            </div>
+            <div className="nav-button">
               <Button variant='info' href={`/responses/${survey._id}`}>View Responses</Button>
+              <Button variant='info' href={`/surveys/${survey._id}`}>View Survey</Button>
             </div>
           </div>
         ))}
