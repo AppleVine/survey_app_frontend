f9621a35c6f27accca5b086c901a947747fd1047
Jakeb <Jakebco99@icloud.com>
2023-08-03 15:34:00 +1000

    Implemented CAPTCHA for survey responses.

--- src/components/survey/SubmitResponseButton.jsx
+++ src/components/survey/SubmitResponseButton.jsx
@@ -1,71 +1,91 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
-import { useResponseContext, useResponseDispatchContext } from '../../contexts/responseContext'
+import React, { useState, useRef } from 'react';
+import { Button } from 'react-bootstrap';
+import { useResponseContext, useResponseDispatchContext } from '../../contexts/responseContext';
 import { useSurveyContext } from '../../contexts/surveyContext';
 import { submitSurveyResponse } from '../../services/responseServices';
+import ReCAPTCHA from "react-google-recaptcha";
+import axios from 'axios';
 
-export default function SubmitResponseButton({submitted, setSubmitted}) {
-    const surveyState = useSurveyContext();
-    const responseState = useResponseContext();
-    const responseDispatch = useResponseDispatchContext();
-    const [clicked, setClicked] = useState(false);
+export default function SubmitResponseButton({ submitted, setSubmitted }) {
+  const surveyState = useSurveyContext();
+  const responseState = useResponseContext();
+  const responseDispatch = useResponseDispatchContext();
+  const [clicked, setClicked] = useState(false);
 
-    const handleSubmitResponse = async () => {
-        if (!clicked) {
-            // Prevent further clicks
-            setClicked(true);
-            // Check that all questions are answered
-            for (let response of responseState.answers) {
-                if (response === null || response === "") {
-                    alert("Please answer all questions before submitting.");
-                    responseDispatch({type: "alert"});
-                    return
-                }
-            }
-            // Make a copy of current state
-            const responseData = structuredClone(responseState);
-            // Remove 'alert'
-            delete responseData.alert;
-            // Get multiple choice questions
-            for (let i = 0; i < surveyState.data.questions.length; i++) {
-                if (surveyState.data.questions[i].data.questionType === "multipleChoiceRadio") {
-                    let optionId =  responseData.answers[i];
-                    // Get the text of the answer from survey state
-                    let fullAnswer = {optionId: optionId, text: surveyState.data.questions[i].data.questionOptions[optionId]};
-                    responseData.answers[i] = fullAnswer;
-                } else if (surveyState.data.questions[i].data.questionType === "multipleChoiceCheckbox") {
-                    let optionArray = responseData.answers[i];
-                    let fullAnswerArray = [];
-                    // For each answer checked, get the option number and text of the answer
-                    for (let j = 0; j < optionArray.length; j++) {
-                        if (responseData.answers[i][j]) {
-                            fullAnswerArray.push({optionId: j, text: surveyState.data.questions[i].data.questionOptions[j]})
-                        }
-                    }
-                    responseData.answers[i] = fullAnswerArray;
-                    }
-                }
-            try {
-                // Submit response data
-                await submitSurveyResponse(responseData)
-                .then(() => {
-                    // Set state of parent component to submitted
-                    setSubmitted(true);
-                })
-            }
-            catch(error) {
-                // Re-enable button clicks
-                setClicked(false);
-                console.error("Error saving response to the database:", error);
-            }
+  const captchaRef = useRef(null);
+
+  const handleSubmitResponse = async () => {
+    if (!clicked) {
+      // Prevent further clicks
+      setClicked(true);
+      // Check that all questions are answered
+      for (let response of responseState.answers) {
+        if (response === null || response === "") {
+          alert("Please answer all questions before submitting.");
+          responseDispatch({ type: "alert" });
+          return;
         }
-    }
+      }
+      // Make a copy of current state
+      const responseData = structuredClone(responseState);
+      // Remove 'alert'
+      delete responseData.alert;
+      // Get multiple choice questions
+      for (let i = 0; i < surveyState.data.questions.length; i++) {
+        if (surveyState.data.questions[i].data.questionType === "multipleChoiceRadio") {
+          let optionId = responseData.answers[i];
+          // Get the text of the answer from survey state
+          let fullAnswer = { optionId: optionId, text: surveyState.data.questions[i].data.questionOptions[optionId] };
+          responseData.answers[i] = fullAnswer;
+        } else if (surveyState.data.questions[i].data.questionType === "multipleChoiceCheckbox") {
+          let optionArray = responseData.answers[i];
+          let fullAnswerArray = [];
+          // For each answer checked, get the option number and text of the answer
+          for (let j = 0; j < optionArray.length; j++) {
+            if (responseData.answers[i][j]) {
+              fullAnswerArray.push({ optionId: j, text: surveyState.data.questions[i].data.questionOptions[j] })
+            }
+          }
+          responseData.answers[i] = fullAnswerArray;
+        }
+      }
 
-    if (submitted) {
-        return(null)
+      const token = captchaRef.current.getValue();
+
+      // Check if the CAPTCHA has been completed
+      if (!token) {
+        alert("Please complete the CAPTCHA.");
+        setClicked(false);
+        return;
+      }
+
+      captchaRef.current.reset();
+
+      try {
+        // Send CAPTCHA data to the backend for validation
+        const response = await axios.post("http://localhost:3000/reCAPTCHA", { token });
+        // Submit response data if CAPTCHA is valid
+        await submitSurveyResponse(responseData);
+        // Set state of parent component to submitted
+        setSubmitted(true);
+      } catch (error) {
+        // Re-enable button clicks
+        setClicked(false);
+        console.error("Error saving response to the database:", error);
+      }
     }
+  };
+
+  if (submitted) {
+    return (null);
+  }
 
   return (
-    <Button variant='primary' onClick={handleSubmitResponse}>Submit Response</Button>
-  )
+    <>
+      <div className="captcha-container">
+        <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} ref={captchaRef} />
+      </div>
+      <Button variant='primary' onClick={handleSubmitResponse}>Submit Response</Button>
+    </>
+  );
 }
--- src/pages/ViewSurvey.jsx
+++ src/pages/ViewSurvey.jsx
@@ -5,83 +5,82 @@ import { getSurvey } from '../services/surveyServices';
 import { checkForUser } from '../services/authServices';
 import ViewSurveyContainer from '../components/survey/ViewSurveyContainer';
 import SubmitResponseButton from '../components/survey/SubmitResponseButton';
+import React from 'react';
+
 
 // CSS imports
 import Button from 'react-bootstrap/Button';
-import { useResponseContext, useResponseDispatchContext } from '../contexts/responseContext';
+import { useResponseDispatchContext } from '../contexts/responseContext';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import LeaveReview from '../components/LeaveReview';
 
+
 export default function ViewSurvey() {
-    // Get survey id from url
-    let { surveyId } = useParams();
-    // Set up dispatch so we can load the survey data into state
-    const dispatch = useSurveyDispatchContext();
-    // Set up response state and dispatch so we can store the responses
-    const responseState = useResponseContext();
-    const responseDispatch = useResponseDispatchContext();
-    // Track whether or not there is a logged in user
-    let isUser = false;
-    // Set up redirects
-    const navigate = useNavigate();
-    // Swap components out when response is submitted
-    const [submitted, setSubmitted] = useState(false);
+  // Get survey id from url
+  let { surveyId } = useParams();
+  // Set up dispatch so we can load the survey data into state
+  const dispatch = useSurveyDispatchContext();
+  // Set up response state and dispatch so we can store the responses
+  const responseDispatch = useResponseDispatchContext();
+  // Track whether or not there is a logged in user
+  let isUser = false;
+  // Set up redirects
+  const navigate = useNavigate();
+  // Swap components out when response is submitted
+  const [submitted, setSubmitted] = useState(false);
 
-    useEffect(() => {
-      // Check if there is a logged in user
-      // eslint-disable-next-line
-      isUser = checkForUser();
-    },[])
 
-    useEffect(() => {
-      // Get survey data and dispatch it into the state
-      const fetchSurvey = async () => {
-        const surveyData = await getSurvey(surveyId);
-        // If 401 unauthorized then redirect to login
-        if (surveyData.response === 401) {
-          navigate('/login');
-        } else {
-          // Reformat question array
-          let questionData = structuredClone(surveyData.survey.questions);
-          let questionArray = [];
-          for (let question of questionData) {
-            question = {data: question};
-            questionArray.push(question);
-          }
-          surveyData.survey.questions = structuredClone(questionArray);
-          dispatch({type: "loadSurvey", data: surveyData.survey});
-          // Create option array for responses
-          let noOfOptions = [];
-          for (let i = 0; i < questionArray.length; i++) {
-            if (questionArray[i].data.questionType === "multipleChoiceCheckbox") {
-              noOfOptions[i] = questionArray[i].data.questionOptions.length;
-            }
-          }
-          responseDispatch({type:"load", data: {surveyId: surveyId, noOfQuestions: questionArray.length, 
-            noOfOptions: noOfOptions}})
+  useEffect(() => {
+    // Check if there is a logged in user
+    // eslint-disable-next-line
+    isUser = checkForUser();
+  },[])
+
+  useEffect(() => {
+    // Get survey data and dispatch it into the state
+    const fetchSurvey = async () => {
+      const surveyData = await getSurvey(surveyId);
+      // If 401 unauthorized then redirect to login
+      if (surveyData.response === 401) {
+        navigate('/login');
+      } else {
+        // Reformat question array
+        let questionData = structuredClone(surveyData.survey.questions);
+        let questionArray = [];
+        for (let question of questionData) {
+          question = {data: question};
+          questionArray.push(question);
         }
+        surveyData.survey.questions = structuredClone(questionArray);
+        dispatch({type: "loadSurvey", data: surveyData.survey});
+        // Create option array for responses
+        let noOfOptions = [];
+        for (let i = 0; i < questionArray.length; i++) {
+          if (questionArray[i].data.questionType === "multipleChoiceCheckbox") {
+            noOfOptions[i] = questionArray[i].data.questionOptions.length;
+          }
+        }
+        responseDispatch({type:"load", data: {surveyId: surveyId, noOfQuestions: questionArray.length, 
+          noOfOptions: noOfOptions}})
       }
-  
-      fetchSurvey()
-      .catch(console.error)
-      // eslint-disable-next-line
-    },[])
+    }
+
+    fetchSurvey()
+    .catch(console.error)
+    // eslint-disable-next-line
+  },[])
 
   return (
     <Col>
-      {submitted ? <LeaveReview />:<ViewSurveyContainer />}
+      {submitted ? <LeaveReview /> : <ViewSurveyContainer />}
       <div>
-        {
-          // Display edit button for logged in users
-          isUser ?
+        {isUser ? (
           <Button variant='primary' href={`/surveys/${surveyId}/edit`} >Edit Survey</Button>
-          :
-          null
-        }
+        ) : null}
       </div>
       <Row className="justify-content-center" >
-        <SubmitResponseButton submitted={submitted} setSubmitted={setSubmitted} />
+        <SubmitResponseButton submitted={submitted} setSubmitted={setSubmitted}/>
       </Row>
     </Col>
   )
